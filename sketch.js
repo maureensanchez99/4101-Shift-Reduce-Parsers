cad3481dd5576587693bb8bfb5feea297c1c25d5
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-04-16 08:27:41 -0500

    added grammar rules

--- sketch.js
+++ sketch.js
@@ -2,10 +2,22 @@ function setup() {
   createCanvas(800, 800);
 }
 
+// this function will draw the table that the parser will use
 function draw() {
   background(220);
 }
 
+// this function will parse the input expression
 function parse(){
-  const input = document.getElementById("inputExpression").value;
+  const input = document.getElementById("inputExpression").value; // grabs the expression that the user inputs
+
+  // grammar rules that the parser has to follow
+  const grammar = [
+    ["E", "E", "+", "T"],
+    ["E", "T"],
+    ["T", "T", "*", "F"],
+    ["T", "F"],
+    ["F", "(", "E", ")"],
+    ["F", "id"]
+  ];
 }
